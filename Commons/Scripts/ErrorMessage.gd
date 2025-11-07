b58eb7d28e1cb47e44de497948723fbd3d6e4a15
PuckInside <saginov606@gmail.com>
2025-11-07 20:46:19 +0500

    Finalized player movement FSM, moved input logic out

--- Commons/Scripts/ErrorMessage.gd
+++ Commons/Scripts/ErrorMessage.gd
@@ -3,4 +3,5 @@ class_name ErrorMessage
 const INVALID_OWNER = "Controller owner is invalid or null."
 const OWNER_TYPE = "Owner must be PlatformerBody2D."
 const NULL_FIELD = "Required field is null or unassigned."
+const NULL_ARGUMENT = "Received null where a valid reference was required."
 const READ_ONLY = "Attempt to modify a read-only property."
